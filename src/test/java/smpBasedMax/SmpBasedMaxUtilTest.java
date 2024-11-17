b733c3f65c21bb3f8cc15372ae0e1e233dd8b04f
anhminh <ahd.minh@gmail.com>
2024-11-18 00:51:30 +0100

    chore(comment): remove redundant comment in test function

--- src/test/java/smpBasedMax/SmpBasedMaxUtilTest.java
+++ src/test/java/smpBasedMax/SmpBasedMaxUtilTest.java
@@ -10,13 +10,11 @@ class SmpBasedMaxUtilTest {
     void sortReturnPeakIndices() {
         SmpBasedMaxUtil smpBasedMaxUtil = new SmpBasedMaxUtil();
 
-        // Test data
         float[] peakValues = {1.2f, 3.4f, 2.5f};
 
-        // Call the method
         int[] result = smpBasedMaxUtil.sortReturnPeakIndices(peakValues);
 
         // Assert that the indices are sorted based on peakValues
-        assertArrayEquals(new int[]{0, 2, 1}, result); // Expected sorted order of indices
+        assertArrayEquals(new int[]{0, 2, 1}, result);
     }
 }
