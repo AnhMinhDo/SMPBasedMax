cf065e346b09dfbdfc9a51122cf81af522eecd13
anhminh <ahd.minh@gmail.com>
2024-11-20 19:02:46 +0100

    test: test for function yUpper1D

--- src/test/java/smpBasedMax/EnvelopeTest.java
+++ src/test/java/smpBasedMax/EnvelopeTest.java
@@ -7,6 +7,15 @@ import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 class EnvelopeTest {
     @Test
+    void testyUpper1D(){
+        float[] signalValues = new float[]{1f,3f,1f,2f,1f,4f,4f,1f};
+        int distance = 1;
+        float[] actual = Envelope.yUpper1D(signalValues, distance);
+        float[] expected = new float[]{4.6250f, 3.0000f, 2.1250f, 2.0000f,2.6250f, 4.0000f,6.1250f,9.0000f};
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
     void testSplineInterpolation() {
         float[] signalValues = new float[]{1f,3f,1f,2f,1f,4f,4f,1f};
         int[] peakIdx = new int[]{1,3,5};
