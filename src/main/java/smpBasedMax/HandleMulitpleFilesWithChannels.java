56c45ac7dbe96e731c86184bb6118493dd7b50d8
anhminh <ahd.minh@gmail.com>
2025-01-29 19:48:16 +0100

    refactor: add attributes

--- src/main/java/smpBasedMax/HandleMulitpleFilesWithChannels.java
+++ src/main/java/smpBasedMax/HandleMulitpleFilesWithChannels.java
@@ -5,25 +5,42 @@ import ij.IJ;
 import java.io.File;
 
 public class HandleMulitpleFilesWithChannels {
-
+    File[] ArrayFileWithChannels;
+    ZStackDirection zStackDirection;
+    int stiffness;
+    int filterSize;
+    int offset;
+    int depth;
     public HandleMulitpleFilesWithChannels(File dirPath,
                                            ZStackDirection zStackDirection,
                                            int stiffness,
                                            int filterSize,
                                            int offset,
                                            int depth) {
-        File[] ArrayFileWithChannels = dirPath.listFiles(File::isDirectory);
-        if (ArrayFileWithChannels == null) {
-            IJ.showMessage("No subdirectory in " + dirPath); return;}
-        for(File fileWithChannel : ArrayFileWithChannels) {
+        this.zStackDirection = zStackDirection;
+        this.stiffness = stiffness;
+        this.filterSize = filterSize;
+        this.offset = offset;
+        this.depth = depth;
+        this.ArrayFileWithChannels = dirPath.listFiles(File::isDirectory);
+        if (this.ArrayFileWithChannels == null) {
+            IJ.showMessage("No subdirectory in " + dirPath);
+        }
+    }
+
+    public void process() {
+        performProcessing();
+    }
+
+    private void performProcessing(){
+        for(File fileWithChannel : this.ArrayFileWithChannels) {
             HandleSingleFileWithChannels hsfwc = new HandleSingleFileWithChannels(fileWithChannel,
-                                                                                    zStackDirection,
-                                                                                    stiffness,
-                                                                                    filterSize,
-                                                                                    offset,
-                                                                                    depth);
+                    this.zStackDirection,
+                    this.stiffness,
+                    this.filterSize,
+                    this.offset,
+                    this.depth);
             hsfwc.process();
         }
-
     }
 }
